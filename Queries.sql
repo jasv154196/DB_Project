6b441040bae621cdb4fd3ff204dc6c3508785168
Antonio Sillero <jasv154196@hotmail.com>
2019-04-28 19:37:01 -0500

    Queries

--- Queries.sql
+++ Queries.sql
@@ -28,6 +28,18 @@ GROUP BY Attack_Name
 ORDER BY SUM(N_Killed) DESC
 LIMIT 5;
 
+#Adding a new Terrorist Attack (No Terrorist Attacks records of 2018)
+SELECT * FROM DB_Project.Terrorism t JOIN DB_Project.Dates d ON t.Date_ID = d.Date_ID
+WHERE Year = 2018;
+
+SELECT * FROM DB_Project.Dates
+WHERE Year = 2018;
+
+INSERT INTO DB_Project.Dates(Date_ID, Year, Month, Day)
+VALUES (18181818, 2018, 5, 1);
+
+INSERT INTO DB_Project.Terrorism(Event_ID, Date_ID, Country_ID, Region_ID, Success, Attack_ID, Target_ID, Org_Name, Weapon_ID, N_Killed)
+VALUES (20182018, 18181818, 217, 1, 1, 3, 8, 'TTU-CS-Databases', 7, 201888);
  
- 
- 
+UPDATE DB_Project.Terrorism SET Success = 0, N_Killed = 0, Org_Name = 'Unknown'
+WHERE DB_Project.Terrorism.Event_ID = 20182018;
